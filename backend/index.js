921ced08117d0a0200b414184cf422df08f74a33
NicolasCarlini <nicolas.carlini@fizzmod.com>
2020-08-28 19:46:09 -0300

    docker base

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("backend");
+const express = require('express');
+const app = express();
+const port = process.env.BKT_PORT;
+
+app.get('/', (req, res) => {
+  res.send('backend!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,11 @@
-console.log("frontend");
+const express = require('express');
+const app = express();
+const port = process.env.FRT_PORT;
+
+app.get('/', (req, res) => {
+  res.send('frontend');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
