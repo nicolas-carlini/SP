f10c6c391540d79ab057979c2a22272187ee47cf
NicolasCarlini <nicolas.carlini@fizzmod.com>
2020-08-29 17:05:27 -0300

    mongo test

--- apiDB/src/index.js
+++ apiDB/src/index.js
@@ -1,9 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.APIDB_PORT;
 const Test = require("./models/test");
 var faker = require('faker');
 require("./database");
 
-module.exports = async () => {
-  
+app.get('/', async (req, res) => {
+
   //peticiones
   let test = new Test();
   
@@ -14,22 +17,12 @@ module.exports = async () => {
   test.password = faker.internet.password();
   test.phone = faker.phone.phoneNumber();
   test.ip = faker.internet.ip();
-  console.log("\033[2J");
-  console.log("Let's go papurri gogogogogogo");
+  
   await test.save();
   
-  return test;
-};
+  res.json(test);
+})
 
-/* 
-//peticiones numericas
-  for (let i = 0; i < 1000; i++) {
-    const lasted = await Test.find().sort({ $natural: -1 }).limit(1);
-    let cant = lasted[0].cant;
-    cant = cant + cant + 1;
-    const test = new Test();
-    test.cant = cant;
-    await test.save();
-    console.log(i + ":" + cant);
-  }
-*/
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
